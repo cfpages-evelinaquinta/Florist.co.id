9c632c2610b5539673c079332522559a214aefb1
aisyahevelina <aisyahevelinaa@gmail.com>
2024-10-18 10:20:27 +0700

    upload website baru florist

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1096.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-je0y974f' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-mzl6oiey' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-yvnf44wh' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-wja5awsn' );
+});
+window.addEventListener( 'load', function() {
+	UAGBTabs.init( '.uagb-block-0ae5c31d' );
+	UAGBTabs.anchorTabId( '.uagb-block-0ae5c31d' );
+});
+window.addEventListener( 'hashchange', function() {
+	UAGBTabs.anchorTabId( '.uagb-block-0ae5c31d' );
+}, false );
+window.addEventListener( 'load', function() {
+	UAGBTabs.init( '.uagb-block-0ae5c31d' );
+	UAGBTabs.anchorTabId( '.uagb-block-0ae5c31d' );
+});
+window.addEventListener( 'hashchange', function() {
+	UAGBTabs.anchorTabId( '.uagb-block-0ae5c31d' );
+}, false );
+ });
